adac3172999b378f078c2261c9773d7783e5f7c7
Honaker <chris98@vt.edu>
2019-05-04 14:07:46 -0400

    basic

--- capture.py
+++ capture.py
@@ -1,1 +1,18 @@
+import socket
+import pika
+import sys
+import time
+import pymongo
+import threading
+import subprocess
+import datetime
 
+username = "Honaker"
+password = "buse"
+    
+credentials = pika.PlainCredentials(username, password)
+connection = pika.BlockingConnection(
+    pika.ConnectionParameters(node, 5672, "/", credentials)
+)
+channel = connection.channel()
+channel.exchange_declare(exchange="Place", exchange_type="direct")
