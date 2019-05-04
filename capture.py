cd30916ebad0df3a4d33841a5d1e36384dfd1195
ethanb98 <ethanb98@vt.edu>
2019-05-04 16:20:33 -0400

    Added pressure sensor

--- capture.py
+++ capture.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import socket
 import pika
 import sys
@@ -6,6 +7,9 @@ import pymongo
 import threading
 import subprocess
 import datetime
+
+# imports for pressure sensor
+import RPi.GPIO as GPIO
 
 username = "Honaker"
 password = "buse"
@@ -16,3 +20,26 @@ connection = pika.BlockingConnection(
 )
 channel = connection.channel()
 channel.exchange_declare(exchange="Place", exchange_type="direct")
+
+
+# Requires infinite while loop, may require multi-threading
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(4,GPIO.IN)
+
+prev_input = 0
+try:
+    while True:
+        # Take the reading
+        input = GPIO.input(4)
+        # If last reading was low and this is high, alert
+        if ((not prev_input) and input):
+            print("Under Pressure")
+            print(input)
+        # Update prev_input
+        prev_input = input
+        # Slight Pause
+        time.sleep(0.1)
+except KeyboardInterrupt:
+    passfinally:
+        GPIO.cleanup()
+
